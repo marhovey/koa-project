1a3073418fbaa5db2f336e755e6f513ba8f05012
mahongwei <hzmahongwei@ceboshi.cn>
2019-04-04 18:50:59 +0800

    init app

--- koa-server/router/routes/helloWord.js
+++ koa-server/router/routes/helloWord.js
@@ -1,7 +1,11 @@
 const router = require('koa-router')();
 
 router.get('/', (ctx, next) => {
-  ctx.body = 'hello World';
+  ctx.body = JSON.stringify({
+    data: 'Hello My First Koa Project',
+    errMsg: '操作成功',
+    errCode: 0
+  });
   next();
 })
 
