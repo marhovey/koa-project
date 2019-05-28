de8b3e5118b3196b07b6efcd8fc3e5b55ddce9ac
mahongwei <hzmahongwei@ceboshi.cn>
2019-05-28 13:56:47 +0800

    init app

--- koa-server/utils/utils.js
+++ koa-server/utils/utils.js
@@ -7,7 +7,7 @@ const utils = {
     data.data = await query(SQL)
     for (var i in data.data) {
       let comCnt = await query(`select count(*) as count from comments where art_id=${data.data[i].art_id};`)
-      data.commentCnt = comCnt[0].count
+      data.data[i].commentCnt = comCnt[0].count
     }
     let count = await query(`select count(*) as count from ${tableName};`)
     data.count = count[0].count
