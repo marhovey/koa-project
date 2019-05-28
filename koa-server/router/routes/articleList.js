4e7730c7cc741d413502a2ed34fc7bc5aa157e05
mahongwei <hzmahongwei@ceboshi.cn>
2019-05-28 13:49:32 +0800

    init app

--- koa-server/router/routes/articleList.js
+++ koa-server/router/routes/articleList.js
@@ -2,7 +2,8 @@ const router = require('koa-router')();
 const utils = require('../../utils/utils');
 
 router.get('/', async (ctx, next) => {
-  let results = await utils.getArtList('art_list');
+  let query = ctx.request.query;
+  let results = await utils.getArtList('art_list', query.pageNum, query.pageSize);
   // let results = await utils.getArtList('list_article', 0, 10);
   ctx.body = JSON.stringify({
     data: results,
