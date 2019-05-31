dc22a29e7b417f36c60e2a88afcdd69b32ecc4ef
mahongwei <hzmahongwei@ceboshi.cn>
2019-05-31 11:35:46 +0800

    init app

--- koa-app/src/utils/utils.js
+++ koa-app/src/utils/utils.js
@@ -1,12 +1,12 @@
 export function dateFormat(time) {
   let date = new Date(time * 1);
   let year = date.getFullYear();
-  let mon = addZero(date.getMonth())
+  let mon = addZero(date.getMonth() + 1)
   let day = addZero(date.getDay())
   let h = addZero(date.getHours())
   let min = addZero(date.getMinutes())
   let sec = addZero(date.getSeconds())
-  return `${year}-${mon + 1}-${day} ${h}:${min}:${sec}`
+  return `${year}-${mon}-${day} ${h}:${min}:${sec}`
 }
 
 function addZero(num) {
